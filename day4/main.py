b9544521befe54b64c0064bc8a9e55ca0a9467bb
neider <tomasz.rutowicz@gmail.com>
2022-12-05 02:04:23 +0100

    added day4

--- day4/main.py
+++ day4/main.py
@@ -1,6 +1,7 @@
 def main():
-    print(OpenFile('input.txt'))
+    OpenFile('input.txt')
     print(count_overlap(OpenFile('input.txt')))
+    print(count_overlapv3(OpenFile('input.txt')))
 def OpenFile(file:str):
     with open (file) as f:
         txt = f.readlines()
@@ -13,29 +14,48 @@ def OpenFile(file:str):
             temp.append(i.split('-'))
         fixed_split.append(temp)
     return fixed_split
-def check_if_overlaps(range1,range2):
-    if not range1:
-        return True
-    if not range2:
-        return False  # non-empty range can't be subset of empty range
-    if len(range1) > 1 and range1.step % range2.step:
-        return False  # must have a single value or integer multiple step
-    return range1.start in range2 and range1[-1] in range2
 
 def count_overlap(occurences:list):
     counter = 0
     for i in occurences:
-        if check_if_overlaps(range(int(i[0][0]),int(i[0][1])),range(int(i[1][0]),int(i[1][1]))) == True:
-
-            print(i)
+        z = set(range(int(i[0][0]), int(i[0][1])+1))
+        y = set(range(int(i[1][0]), int(i[1][1])+1))
+        if z.issubset(y) == True:
             counter += 1
-        elif check_if_overlaps(range(int(i[1][0]),int(i[1][1])),range(int(i[0][0]),int(i[0][1]))) == True:
-            print(i)
+        elif y.issubset(z) == True:
             counter += 1
     return counter
 
-    #return [x.split('-') for x in splitted]
 
+def count_overlapv3(occurences:list):
+    counter = 0
+    for i in occurences:
+        z = set(range(int(i[0][0]), int(i[0][1])+1))
+        y = set(range(int(i[1][0]), int(i[1][1])+1))
+        if bool(z & y) == True:
+            counter += 1
+    return counter
 
+'''
+I did misunderstood the solution but i am leavin it in here as it looks cool, might be useful. 
+
+def count_overlap_v2(occurences:list):
+    counter = 0
+    pairs_only = []
+    for item in occurences:
+        for pair in item:
+            pairs_only.append(pair)
+
+    for index, item in enumerate(pairs_only):
+        z = set(range(int(item[0]), int(item[1]) + 1))
+        set_list = pairs_only[:index] + pairs_only[index+1:]
+        for item_v2 in set_list:
+            y = set(range(int(item_v2[0]), int(item_v2[1]) + 1))
+            if z.issubset(y) == True:
+                counter += 1
+            elif y.issubset(z) == True:
+                counter += 1
+    return counter
+'''
 if __name__ == '__main__':
     main()
